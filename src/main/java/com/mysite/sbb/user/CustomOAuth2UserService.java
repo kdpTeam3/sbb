5fed348b8943b3eb7b0fefd4008bf1086780669f
user <user@DESKTOP-T996GA4>
2024-08-28 11:12:38 +0900

    naver/login

---
+++ src/main/java/com/mysite/sbb/user/CustomOAuth2UserService.java
@@ -0,0 +1,34 @@
+package com.mysite.sbb.user;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oauth2User = delegate.loadUser(userRequest);
+
+        Map<String, Object> attributes = oauth2User.getAttributes();
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        String email = (String) response.get("email");
+
+        List<GrantedAuthority> authorities = Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new DefaultOAuth2User(authorities, response, "email");
+    }
+}
